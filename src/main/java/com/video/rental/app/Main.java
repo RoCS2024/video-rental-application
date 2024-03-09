41978b95ea545e0fe1ce29c886848e2691d842fd
unknown <donnie.anciro@gmail.com>
2024-03-09 04:59:34 -0800

    Adds ant and ivy files

--- src/main/java/com/video/rental/app/Main.java
+++ src/main/java/com/video/rental/app/Main.java
@@ -1,4 +1,13 @@
 package com.video.rental.app;
 
+import com.video.rental.app.data.connection.ConnectionHelper;
+
+import java.sql.Connection;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        Connection connection = ConnectionHelper.getConnection();
+    }
 }
--- src/main/java/com/video/rental/app/connection/ConnectionHelper.java
+++
@@ -1,4 +0,0 @@
-package com.video.rental.app.connection;
-
-public class ConnectionHelper {
-}
